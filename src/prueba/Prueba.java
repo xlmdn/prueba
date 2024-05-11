6bb2db6513cdb1da9c1874a633b32cd845223a85
xlmdn <22030321@itesa.edu.mx>
2024-05-11 16:08:35 -0500

    carga inicial

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -11,7 +11,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("holamundo");
     }
     
 }
