a79952dbf0e8cd074a1bf4add8ab7532fa566159
xlmdn <22030321@itesa.edu.mx>
2024-05-11 15:56:07 -0500

    carga inicial

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,17 @@
+
+package prueba;
+
+/**
+ *
+ * @author Axel Madin
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
